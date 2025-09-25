79d3059b68f8f9efb0d6097931b5d870357634f7
Kranthi0307 <kranthia24@gmail.com>
2025-09-25 09:12:26 -0300

    API call enabled

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { FooterComponent } from "../footer/footer.component";
 import { HeaderComponent } from "../header/header.component";
 import { ProjectsService } from './projects.service';
+import { LoadingService } from '../common/services/loading.service';
 
 @Component({
   selector: 'app-projects',
@@ -13,40 +14,20 @@ import { ProjectsService } from './projects.service';
 })
 export class ProjectsComponent implements OnInit {
 
-  constructor(private projectsService: ProjectsService) { }
+  projects: any = [];
+  loading$: any;
 
-  ngOnInit(): void {
-    this.projectsService.getProjects().subscribe({
-      next: (response: any) => console.log(response),
-      error: (error: any) => console.log(error)
-    });
+  constructor(private projectsService: ProjectsService,
+    private readonly loadingService: LoadingService
+  ) {
+    this.loading$ = this.loadingService.loading$;
   }
 
-  projects = [
-    {
-      name: 'My Portfolio',
-      description: 'This is a display of my expertise, seamlessly showcasing in action.',
-      link: 'https://github.com/Kranthi0307/portefeuille'
-    },
-    {
-      name: 'B2BKaro',
-      description: 'An e-commerce website that I am contributing as a freelancer.',
-      link: 'https://b2bkaro.com/b2bkaro/login'
-    },
-    {
-      name: 'Boulanger',
-      description: 'An e-commerce related insurance wesite that I am contributing as a freelancer.',
-      link: 'https://www.boulanger.com/'
-    },
-    {
-      name: 'rendez-vous',
-      description: 'A website that is used for seamless booking of appointments to specific doctor. This is an under-developed website which is yet to be deployed.',
-      link: 'https://github.com/Kranthi0307'
-    },
-    {
-      name: 'elasticsearch',
-      description: 'An open source project that I am exploring to contribute.',
-      link: 'https://github.com/Kranthi0307/elasticsearch'
-    }
-  ]
+  ngOnInit(): void {
+    this.loadingService.show();
+    this.projectsService.getProjects().subscribe({
+      next: (response: any) => { this.projects = response, this.loadingService.hide() },
+      error: (error: any) => { console.log(error), this.loadingService.hide() }
+    });
+  }
 }
