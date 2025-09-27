7cc30508ee6d212f570528371a96647adf1073ab
Kranthi0307 <kranthia24@gmail.com>
2025-09-27 14:06:12 -0300

    skills ui updated

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -26,8 +26,24 @@ export class SkillsComponent implements OnInit {
   ngOnInit(): void {
     this.loadingService.show();
     this.skillsService.getSkills().subscribe({
-      next: (response: any) => { this.skills = response, this.loadingService.hide() },
+      next: (response: any) => { this.skills = this.groupBySkill(response), console.log(this.skills), this.loadingService.hide() },
       error: (error: any) => { console.log(error), this.loadingService.hide() }
     });
+  }
+
+  private groupBySkill(response: { name: string, label: string }[]) {
+    const result: { [key: string]: string[] } = {};
+
+    for (let skill of response) {
+      if (!result[skill.label]) {
+        result[skill.label] = [];
+      }
+      result[skill.label].push(skill.name);
+    }
+
+    return Object.keys(result).map(skill => ({
+      label: skill,
+      names: result[skill]
+    }));
   }
 }
