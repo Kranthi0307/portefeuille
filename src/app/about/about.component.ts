f3f8d09555083c887f272b491fcd04450c1da72c
Kranthi <66398071+Kranthi0307@users.noreply.github.com>
2025-10-04 16:36:53 -0300

    Update about.component.ts

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -17,13 +17,13 @@ export class AboutComponent implements OnInit {
 
   ngOnInit(): void {
     this.work = [
-      { "title": "Full-stack Developer", "company": "Remote", "description": "Test-1" },
-      { "title": "Exam Support Assistant | Student Accessibility Center", "company": "Dalhousie University", "description": "Test-2" },
-      { "title": "Software Developer", "company": "SoWeDid Solutions", "description": "Test-3" }
+      { "title": "Full-stack Developer", "company": "Remote", "description": "Working as a Full Stack Developer, building scalable, high-performance web applications using modern frameworks and microservices-based architectures. Involved in the complete software development lifecycle from system design and development to deployment and monitoring." },
+      { "title": "Exam Support Assistant | Student Accessibility Center", "company": "Dalhousie University", "description": "I provide direct support to students registered with the Student Accessibility Centre during exams, ensuring equitable access and a fair testing environment. My role focuses on fostering inclusion, maintaining academic integrity, and delivering student-centred support." },
+      { "title": "Software Developer", "company": "SoWeDid Solutions", "description": "As a Software Developer Trainee, I contributed to full-stack application development, working across backend, frontend, and database layers. I gained hands-on experience with modern web technologies and collaborated with cross-functional teams to enhance system performance and scalability." }
     ];
     this.education = [
-      { "school": "Dalhousie University", "degree": "Master of Engineering - MEng", "field": "Internetworking", "description": "Test-1", "startDate": "September 2023", "endDate": "May 2025" },
-      { "school": "R.M.D ENGINEERING COLLEGE", "degree": "Bachelor of Engineering - BE", "field": "Electrical and Electronics Engineering", "description": "Test-2", "startDate": "June 2016", "endDate": "September 2020" }
+      { "school": "Dalhousie University", "degree": "Master of Engineering - MEng", "field": "Internetworking", "description": "Pursued a comprehensive program focused on the design, implementation, and management of complex networked systems. Gained in-depth knowledge of networking technologies, protocols, and architectures, along with hands-on experience in secure and scalable network design.", "startDate": "September 2023", "endDate": "May 2025" },
+      { "school": "R.M.D ENGINEERING COLLEGE", "degree": "Bachelor of Engineering - BE", "field": "Electrical and Electronics Engineering", "description": "Developed a strong technical foundation in electrical and electronics engineering with a deep interest in laboratory work, programming, and practical experimentation. Coursework emphasized the design, analysis, and implementation of electrical and electronic circuits, renewable energy systems, and automation technologies.", "startDate": "June 2016", "endDate": "September 2020" }
     ];
   }
 }
