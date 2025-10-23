6dba0eb509a360f09a7215a302b54daef2b37bd4
Kranthi0307 <kranthia24@gmail.com>
2025-10-23 09:11:07 -0300

    Resume added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { FooterComponent } from "../footer/footer.component";
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from "../header/header.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule, FooterComponent, HeaderComponent],
+  imports: [RouterModule, FooterComponent, HeaderComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  showTooltip = false;
+  updatedDate = new Date(2025, 8);
 }
