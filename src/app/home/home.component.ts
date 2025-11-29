12cda692ba5135055d9a3486d8e63ce518b6c5b8
Kranthi0307 <kranthia24@gmail.com>
2025-11-29 13:34:13 -0400

    home or dasboard upgraded

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,44 +1,89 @@
-import { Component, HostListener } from '@angular/core';
-import { FooterComponent } from "../footer/footer.component";
-import { RouterModule } from '@angular/router';
-import { HeaderComponent } from "../header/header.component";
 import { CommonModule } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import imagesData from '../../assets/data/image.json';
+import { IImage } from '../common/models/image';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule, FooterComponent, HeaderComponent, CommonModule],
+  imports: [RouterModule, CommonModule, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
-  showTooltip = false;
-  tooltipLocked = false;
-  updatedDate = new Date(2025, 8);
+export class HomeComponent implements OnInit, OnDestroy {
 
-  onMouseEnter() {
-    if (!this.tooltipLocked) {
-      this.showTooltip = true;
+  images: IImage[] = imagesData;
+  currentIndex = 0;
+  interval: any;
+
+  startX = 0;
+  currentX = 0;
+  isDragging = false;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.startAutoPlay();
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.interval);
+  }
+
+  startAutoPlay() {
+    this.interval = setInterval(() => {
+      this.nextSlide();
+    }, 25000);
+  }
+
+  resetAutoPlay() {
+    clearInterval(this.interval);
+    this.startAutoPlay();
+  }
+
+  nextSlide() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+
+  prevSlide() {
+    this.currentIndex =
+      this.currentIndex === 0 ? this.images.length - 1 : this.currentIndex - 1;
+  }
+
+  goToSlide(i: number) {
+    this.currentIndex = i;
+    this.resetAutoPlay();
+  }
+
+  onTouchStart(event: any) {
+    this.isDragging = true;
+    this.startX = event.touches ? event.touches[0].clientX : event.clientX;
+    this.currentX = this.startX;
+    clearInterval(this.interval); // pause autoplay while dragging
+  }
+
+  onTouchMove(event: any) {
+    if (!this.isDragging) return;
+    this.currentX = event.touches ? event.touches[0].clientX : event.clientX;
+  }
+
+  onTouchEnd() {
+    if (!this.isDragging) return;
+
+    const deltaX = this.currentX - this.startX;
+
+    if (Math.abs(deltaX) > 50) {
+      if (deltaX < 0) this.nextSlide();
+      else this.prevSlide();
     }
+
+    this.isDragging = false;
+    this.resetAutoPlay();
   }
 
   onMouseLeave() {
-    if (!this.tooltipLocked) {
-      this.showTooltip = false;
-    }
-  }
-
-  toggleTooltip() {
-    this.tooltipLocked = !this.tooltipLocked;
-    this.showTooltip = this.tooltipLocked || this.showTooltip;
-  }
-
-  @HostListener('document:click', ['$event'])
-  closeTooltip(event: Event) {
-    const target = event.target as HTMLElement;
-    if (!target.closest('.info-icon')) {
-      this.tooltipLocked = false;
-      this.showTooltip = false;
-    }
+    if (this.isDragging) this.onTouchEnd();
   }
 }
